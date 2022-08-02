1f15ebef56525255b066bb37ae69348cd1162f5d
rodeok <nationx22@gmail.com>
2022-08-02 15:48:07 +0100

    beginner☑️

--- datatypes.py
+++ datatypes.py
@@ -1,2 +1,28 @@
 # Data types in python
 
+# types of data types 
+x = "Hello World" #string
+x = 20	#integer
+x = 20.5 #float
+x = 1j	#complex number
+""""
+ A complex number is the sum
+  of a real number and an imaginary number.
+   A complex number is of the form a + ib and 
+   is usually represented by z. Here both a and
+    b are real numbers. The value 'a' is called 
+    the real part which is denoted by Re(z), and 'b'
+     is called the imaginary part Im(z). 
+      Also, ib is called an imaginary number
+"""
+x = ["apple", "banana", "cherry"]	#list
+x = ("apple", "banana", "cherry")	#tuple
+x = range(6)	#range
+x = {"name" : "John", "age" : 36}	#dictionary
+x = {"apple", "banana", "cherry"}	#set
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True	#bool
+x = b"Hello"	#bytes
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview	
+x = None	#NoneType	
