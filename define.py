b0cc357b3d5cdc5afe4ed7581c4bd11f95bca4a1
humble327 <humblehenry1@gmail.com>
2022-08-03 07:37:51 -0700

    day 2

---
+++ define.py
@@ -0,0 +1,22 @@
+# g =8
+# k =4.00
+# h ="door"
+
+# a =str (g)
+# b =int (k)
+# # c =float(h)
+
+# print(g)
+# print(k)
+# print(h)
+
+x = input("firstname:")
+y = input("lastname: ")
+
+# x = int(x)
+# y = int(y)
+
+if x < y :
+    print("x is less than y:")
+    print()
+    type()
