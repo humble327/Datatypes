c92a5bef6cb1a6d7238cf526304d29ab018c6472
rodeok <nationx22@gmail.com>
2022-08-03 12:56:17 +0100

    G

--- data.py
+++ data.py
@@ -23,23 +23,48 @@
 # print(type(y))
 # print(type(z))
 
-x = 1    # int 1.0
-y = 2.8  # float 2
-z = 1j   # complex 1 +0j
+# x = 1    # int 1.0
+# y = 2.8  # float 2
+# z = 1j   # complex 1 +0j
 
-#convert from int to float:
-a = float(x)
+# #convert from int to float:
+# a = float(x)
 
-#convert from float to int:
-b = int(y)
+# #convert from float to int:
+# b = int(y)
 
-#convert from int to complex:
-c = complex(x)
+# #convert from int to complex:
+# c = complex(x)
 
-print(a)
-print(b)
-print(c)
+# print(a)
+# print(b)
+# print(c)
 
-print(type(a))
-print(type(b))
-print(type(c))
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+
+
+
+
+
+import random
+
+print(random.randrange(7, 10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
