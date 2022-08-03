9da6b581067afe5492357a57d2a98b70905e28f5
rodeok <nationx22@gmail.com>
2022-08-03 15:36:00 +0100

    Day two done conditional statement☑️

---
+++ ifelse.py
@@ -0,0 +1,19 @@
+# x = input("first name: ")
+# y = input("lastname: ")
+# # x
+
+
+# if x < y:
+#     print("x is less than y!")
+# else:
+#     print("x is not less than y")   
+
+x = input("number: ")
+y = input("number: ")
+
+if x < y:
+    print("x is less than one")
+else:
+    print("x is not less than one ")   
+# ==
+# *
